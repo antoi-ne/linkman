6fb2bf3ca46120273322ea5c0ec17ff1995836ca
Antoine Coulon <antoine@coulon.dev>
2024-04-14 01:55:02 +0200

    srv

--- server.py
+++ server.py
@@ -3,39 +3,47 @@ import selectors
 import link
 import pymikro.src.pymikro as pymikro
 
-class ClientManager():
-    def __init__(self):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.setblocking(False)
-        server_address = ('0.0.0.0', 7777)
-        print('starting up on', server_address)
-        self.server_socket.bind(server_address)
-        self.server_socket.listen(5)
 
-        self.selector = selectors.DefaultSelector()
+class ClientManager:
+    def __init__(self, addr=""):
 
-        self.selector.register(self.server_socket, selectors.EVENT_READ)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setblocking(False)
+
+        if not addr:
+            addr = ("0.0.0.0", 7777)
+            print("listening on", addr)
+            self.socket.bind(addr)
+            self.socket.listen(32)
+            self.selector = selectors.DefaultSelector()
+            self.selector.register(self.socket, selectors.EVENT_READ)
+        else:
+            print("connecting to", addr)
+            try:
+                self.socket.connect((addr, 7777))
+            except BlockingIOError:
+                pass
 
     def _accept_wrapper(self, sock):
         client_socket, client_address = sock.accept()
-        print('connection from', client_address)
+        print("connection from", client_address)
         client_socket.setblocking(False)
         self.selector.register(client_socket, selectors.EVENT_READ, data=client_address)
 
     def _close_wrapper(self, client_socket):
         self.selector.unregister(client_socket)
         client_socket.close()
-    
+
     def _handle_client(self, client_socket, message):
         try:
             client_socket.sendall(message)
         except Exception as e:
-            print('error occurred:', e) 
-    
+            print("error occurred:", e)
+
     def send(self, msg):
         while len((events := self.selector.select(timeout=0))):
             for selector_key, _ in events:
-                if selector_key.fileobj == self.server_socket:
+                if selector_key.fileobj == self.socket:
                     self._accept_wrapper(selector_key.fileobj)
                 else:
                     self._close_wrapper(selector_key.fileobj)
@@ -43,7 +51,15 @@ class ClientManager():
         for _, selector_key in self.selector.get_map().items():
             if selector_key.data is not None:
                 client_socket = selector_key.fileobj
-                self._handle_client(client_socket, bytes(msg, encoding='utf-8'))
+                self._handle_client(client_socket, msg)
+
+    def recv(self):
+        try:
+            data = self.socket.recv(1024)
+        except BlockingIOError:
+            return None
+        else:
+            return data
 
 
 m_colors = [
@@ -70,11 +86,11 @@ class Maschine:
     def __init__(self) -> None:
         self.m = pymikro.MaschineMikroMk3()
         self.m.setScreen("linkman", 28)
-        self.m.setLight('button', 'pad_mode')
-        self.m.setLight('button', 'step')
+        self.m.setLight("button", "pad_mode")
+        self.m.setLight("button", "step")
         self.selected_color = "white"
         self.shift_pressed = False
-        self.mode = 'pattern'
+        self.mode = "pattern"
         self.beats = [
             "off",
             "off",
@@ -108,36 +124,37 @@ class Maschine:
             if i == phase:
                 lvl = 3
             self.m.setLight("pad", i, lvl, m.beats[i])
-            if i == phase and m.beats[i] == 'off':
-                self.m.setLight("pad", i, 4, 'white')
+            if i == phase and m.beats[i] == "off":
+                self.m.setLight("pad", i, 4, "white")
         self.m.updLights()
 
     def read(self):
         cmd = self.m.readCmd()
         if cmd:
-            if self.mode == 'color':
+            if self.mode == "color":
                 if cmd["cmd"] == "pad":
                     self.selected_color = m_colors[cmd["pad_nb"]]
                 if cmd["cmd"] == "btn":
-                    if 'step' in cmd["btn_pressed"]:
-                        self.mode = 'pattern'
-                    if 'shift' in cmd["btn_pressed"]:
+                    if "step" in cmd["btn_pressed"]:
+                        self.mode = "pattern"
+                    if "shift" in cmd["btn_pressed"]:
                         self.shift_pressed = True
                     else:
                         self.shift_pressed = False
-            elif self.mode == 'pattern':
+            elif self.mode == "pattern":
                 if cmd["cmd"] == "pad":
                     if self.shift_pressed == True:
-                        self.beats[cmd["pad_nb"]] = 'off'
+                        self.beats[cmd["pad_nb"]] = "off"
                     else:
                         self.beats[cmd["pad_nb"]] = self.selected_color
                 if cmd["cmd"] == "btn":
-                    if 'pad_mode' in cmd["btn_pressed"]:
-                        self.mode = 'color'
-                    if 'shift' in cmd["btn_pressed"]:
+                    if "pad_mode" in cmd["btn_pressed"]:
+                        self.mode = "color"
+                    if "shift" in cmd["btn_pressed"]:
                         self.shift_pressed = True
                     else:
                         self.shift_pressed = False
+
 
 l = link.Link(120.0)
 l.enabled = True
@@ -164,9 +181,9 @@ while 1:
     phase_pulse = int(phase * 4)
 
     m.read()
-    if m.mode == 'pattern':
+    if m.mode == "pattern":
         m.set_pattern_mode(phase_pulse)
-    elif m.mode == 'color':
+    elif m.mode == "color":
         m.set_color_mode()
 
     if quantized == False:
@@ -175,11 +192,10 @@ while 1:
         else:
             continue
 
-    pattern = "".join(m.beats)
+    pattern = ",".join(m.beats)
 
     if pattern != last_pattern:
         cm.send(bytes(pattern))
         last_pattern = pattern
 
     last_pulse = beat_pulse
-
