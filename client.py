684177739c0529608216c836f72deb18cd56b6e8
Antoine Coulon <antoine@coulon.dev>
2024-04-14 01:53:36 +0200

    test

--- client.py
+++ client.py
@@ -3,6 +3,66 @@ import sdl2
 import link
 import pymikro.src.pymikro as pymikro
 
+import socket
+import selectors
+
+class ClientManager():
+    def __init__(self, addr=''):
+        
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setblocking(False)
+
+        if not addr:
+            addr = ('0.0.0.0', 7777)
+            print('listening on', addr)
+            self.socket.bind(addr)
+            self.socket.listen(32)
+            self.selector = selectors.DefaultSelector()
+            self.selector.register(self.socket, selectors.EVENT_READ)
+        else:
+            print('connecting to', addr)
+            try:
+                self.socket.connect((addr, 7777))
+            except BlockingIOError:
+                pass 
+
+    def _accept_wrapper(self, sock):
+        client_socket, client_address = sock.accept()
+        print('connection from', client_address)
+        client_socket.setblocking(False)
+        self.selector.register(client_socket, selectors.EVENT_READ, data=client_address)
+
+    def _close_wrapper(self, client_socket):
+        self.selector.unregister(client_socket)
+        client_socket.close()
+    
+    def _handle_client(self, client_socket, message):
+        try:
+            client_socket.sendall(message)
+        except Exception as e:
+            print('error occurred:', e) 
+    
+    def send(self, msg):
+        while len((events := self.selector.select(timeout=0))):
+            for selector_key, _ in events:
+                if selector_key.fileobj == self.socket:
+                    self._accept_wrapper(selector_key.fileobj)
+                else:
+                    self._close_wrapper(selector_key.fileobj)
+
+        for _, selector_key in self.selector.get_map().items():
+            if selector_key.data is not None:
+                client_socket = selector_key.fileobj
+                self._handle_client(client_socket, msg)
+    
+    def recv(self):
+        try:
+            data = self.socket.recv(1024)
+        except BlockingIOError:
+            return None
+        else:
+            return data
+
 black = sdl2.ext.Color(0, 0, 0)
 
 
@@ -107,6 +167,8 @@ beats = [
     "off",
 ]
 
+cm = ClientManager("172.20.10.2")
+
 while 1:
 
     s = l.captureSessionState()
@@ -129,6 +191,10 @@ while 1:
             quantized = True
         else:
             continue
+
+    b = cm.recv()
+    if b != None:
+        beats = str(b).split(",")
 
     if beat_pulse > last_pulse:
         w.set_color(color_to_sdl(beats[phase_pulse]))
