d12cb26301d4c43cb64ab0954ff79cc97ab45a32
suske <suske@s19.be>
2024-05-24 18:03:30 +0200

    cooking

--- test.py
+++ test.py
@@ -9,11 +9,25 @@ class Size:
     width: int
     height: int
 
-win = Window(size=Size(600, 400))
+win = Window(size=Size(600, 375), full_screen=False)
 
 i = True
+win.set_color('black')
+
+def button_grid(x, y, size, padding, count):
+    return [win.add_rect(
+        x + ((size + padding) * (i % count)),
+        y + ((size + padding) * int(i / count)),
+        size,
+        size,
+        'white') 
+        for i in range(count ** 2)]
+
+rects = button_grid(275, 50, 50, 25, 4)
+win.render()
 while not win.should_close():
     sleep(1)
+    win.render()
 
 
 win.terminate()
--- window.py
+++ window.py
@@ -1,5 +1,7 @@
 import glfw
 from OpenGL.GL import *
+from dataclasses import dataclass
+from typing import List
 
 colors = {
     "red": (255, 0, 0),
@@ -20,22 +22,29 @@ colors = {
     "black": (0, 0, 0),
 }
 
-def convert_color(func):
-    def wrapper(self, color):
-        if type(color) == str:
-            color = colors.get(color, colors['black'])
-        func(self, color)
-    return wrapper
+def convert_color(color):
+    if type(color) == str:
+        color = colors.get(color, colors['black'])
+    color = [i / 255 for i in color]
+    return color
+
+@dataclass
+class Rect:
+    x: int
+    y: int
+    width: int
+    height: int
+    color: List[float]
 
 class Window:
 
-    def __init__(self, title="Linkman", size=None):
+    def __init__(self, title="Linkman", size=None, full_screen=True):
         if not glfw.init():
             print("Failed to initialize gflw")
             exit(1)
-        if not size:
+        if not size or full_screen:
             size = glfw.get_video_mode(glfw.get_primary_monitor()).size
-        self.window = glfw.create_window(size.width, size.height, title, glfw.get_primary_monitor(), None)
+        self.window = glfw.create_window(size.width, size.height, title, glfw.get_primary_monitor() if full_screen else None, None)
 
         if not self.window:
             print("Failed to open window")
@@ -43,26 +52,50 @@ class Window:
             return
 
         glfw.make_context_current(self.window)
-        glfw.set_key_callback(self.window, self._exit_callback)
+        glfw.set_key_callback(self.window, self._key_callback)
+        glClearColor(0, 0, 0, 1)
+        glClear(GL_COLOR_BUFFER_BIT)
         glfw.swap_buffers(self.window)
         glfw.set_input_mode(self.window, glfw.CURSOR, glfw.CURSOR_HIDDEN)
 
-        self.color = (0, 0, 0)
+        #allow to draw in pixels and not percentages
+        glMatrixMode(GL_PROJECTION)
+        glOrtho(0, size.width, size.height, 0, -1, 1)
+        glMatrixMode(GL_MODELVIEW)
 
-    def _exit_callback(self, window, key, scancode, action, mods):
+        self.bg = (0, 0, 0)
+        self.rects = []
+
+    def _key_callback(self, window, key, scancode, action, mods):
         if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
             glfw.set_window_should_close(self.window, True)
 
-
     def render(self):
-        glClearColor(*self.color, 1.0)
+        glClearColor(*self.bg, 1.0)
         glClear(GL_COLOR_BUFFER_BIT)
+        for r in self.rects:
+            self.draw_rect(r)
         glfw.swap_buffers(self.window)
 
-    @convert_color
     def set_color(self, color):
-        self.color = color
-        self.render()
+        self.bg = convert_color(color)
+
+    def draw_rect(self, r):
+        glBegin(GL_QUADS)
+        glColor(*r.color)
+        glVertex(r.x, r.y)
+        glVertex(r.x + r.width, r.y)
+        glVertex(r.x + r.width, r.y + r.height)
+        glVertex(r.x, r.y + r.height)
+        glEnd()
+
+    def add_rect(self, x, y, width, height, color):
+        r = Rect(x, y, width, height, list(convert_color(color)))
+        self.rects.append(r)
+        return r
+
+    def remove_rect(self, r):
+        return self.rects.remove(r)
 
     def poll_events(self):
         glfw.poll_events()
